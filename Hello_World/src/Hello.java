457fd7bb7deb217c0deb6628e00663f9fae65eab
sowmik-sec <sowmik.sec@gmail.com>
2022-11-19 11:07:15 +0600

    Let's start the course

---
+++ Hello_World/src/Hello.java
@@ -0,0 +1,11 @@
+
+public class Hello {
+	public static void main(String args[]) {
+		System.out.println("Hello Wrold.");
+		int value = 0;
+		System.out.println(value);
+		float f = 9f;
+		double d = 23;
+	}
+
+}
